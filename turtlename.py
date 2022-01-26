3a3b0103ea682cf471ae913041bf5b7e8dd0bd71
Aidan Prior <priorlyaidan@yahoo.com>
2022-01-26 10:31:13 -0500

    jan 26 class

--- turtlename.py
+++ turtlename.py
@@ -1,19 +1,85 @@
 from turtle import *
 import math
 
-height, width, kerning = 100, 50, 10
 
-def angledLine(angle, x, y):
-    left(angle)
-    forward(math.sqrt(x**2 + y**2))
-
-
-def a():
-    angledLine(65, width/2, height)
-    angledLine(0, width/2, height)
+uppercase = (50, 100)
+lowercase = (40, 80)
+kerning = 20
 
 
+mode("logo")
+pensize(10)
+hideturtle()
 
-a()
+
+
+def a(dim):
+    width, height = dim
+    p = pos()
+    pendown()
+    goto(p + (width/2, height))
+    goto(p + (width, 0))
+    penup()
+    goto(p + (width/4, height/2))
+    pendown()
+    goto(p + (width*3/4, height/2))
+    penup()
+    goto(p + (width, 0))
+
+def i(dim):
+    width, height = dim
+    p = pos()
+    penup()
+    goto(p + ( width*(1/5)  , height ) )
+    pendown()
+    goto(p + ( width*(4/5)  , height ) )
+    goto(p + ( width/2      , height ) )
+    goto(p + ( width/2      , 0      ) )
+    goto(p + ( width*(1/5)  , 0      ) )
+    goto(p + ( width*(4/5)  , 0      ) )
+    penup()
+    goto(p + (width, 0))
+
+def d(dim):
+    width, height = dim
+    p = pos()
+    pendown()
+    goto(p + ( 0    , height ) )
+    right(90)
+    circle(-height/2, 180)
+    penup()
+    goto(p + ( width, 0 ) )
+
+def n(dim):
+    width, height = dim
+    p = pos()
+    pendown()
+    goto(p + ( 0, height ) ) 
+    goto(p + ( width, 0) )
+    goto(p + ( width, height) )
+    penup()
+    goto(p + ( width, 0) )
+
+
+def space():
+    goto(pos() + (kerning, 0))
+    
+
+
+speed(1)
+pencolor("red")
+
+goto(uppercase[0] + 4*lowercase[0] , 0)
+
+a(lowercase)
+space()
+i(uppercase)
+space()
+d(lowercase)
+space()
+a(uppercase)
+space()
+n(lowercase)
+
 
 done()
